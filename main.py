3b5d45fcca495db1ff83e33be8e72d30cd4e6fc5
meyupy <meyupy00@gmail.com>
2024-04-29 19:24:51 +0300

    Initial commit

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ pygame.display.set_caption("2x2 Rubik's Cube Solver")
 bg_panel = pygame.rect.Rect(0, 0, 3 * W // 4, 9 * W // 16)
 clock = pygame.time.Clock()
 
-NOTATIONS = ['F', 'F2', "F'", 'U', 'U2', "U'", 'R', 'R2', "R'"]
+NOTATIONS = ["F ", "F2", "F'", "U ", "U2", "U'", "R ", "R2", "R'"]
 
 
 class Button:
@@ -61,13 +61,25 @@ class Button:
         self.surface.blit(self.text_surf, self.text_rect)
 
 
+def convert_path_into_display_text(solution_path):
+    solution_text = ""
+    for n in range(0, len(solution_path), 2):
+        text_part = solution_path[n:n + 2]
+        solution_text += text_part
+        if text_part[1] == " ":
+            solution_text += " "
+        else:
+            solution_text += "  "
+    return solution_text[:-2]
+
+
 def produce_random_scramble():
-    random_scramble = [random.choice(NOTATIONS)]
-    scramble_length = random.randint(9, 10)
-    while len(random_scramble) < scramble_length:
+    random_scramble = random.choice(NOTATIONS)
+    num_notations_in_scramble = random.randint(9, 10)
+    while len(random_scramble) < num_notations_in_scramble * 2:
         random_notation = random.choice([the_notation for the_notation in NOTATIONS
-                                         if the_notation[0] != random_scramble[-1][0]])
-        random_scramble.append(random_notation)
+                                         if the_notation[0] != random_scramble[-2]])
+        random_scramble += random_notation
     return random_scramble
 
 
@@ -116,55 +128,57 @@ def produce_permutation_for_turner_buttons(notation, perm):
 
 
 def produce_permutation_from_path(perm, path):
-    for notation in path:
+    for n in range(0, len(path), 2):
+        notation = path[n:n+2]
         perm = produce_permutation(notation, perm)
     return perm
 
 
 def produce_permutations_from_path(perm, path):
     perms = [perm]
-    for notation in path[:-1]:
+    for n in range(0, len(path) - 2, 2):
+        notation = path[n:n + 2]
         perm = produce_permutation(notation, perm)
         perms.append(perm)
     return perms
 
 
 def produce_permutation(notation, perm):
-    [f1, f2, f3, f4, u1, u2, u3, u4, r1, r2, r3, r4,
-     b1, b2, b3, b4, d1, d2, d3, d4, l1, l2, l3, l4] = perm
-    if notation == "F":
-        perm = [f3, f1, f4, f2, u1, u2, l4, l2, u3, r2, u4, r4,
-                b1, b2, b3, b4, r3, r1, d3, d4, l1, d1, l3, d2]
+    (f1, f2, f3, f4, u1, u2, u3, u4, r1, r2, r3, r4,
+     b1, b2, b3, b4, d1, d2, d3, d4, l1, l2, l3, l4) = perm
+    if notation == "F ":
+        perm = f3 + f1 + f4 + f2 + u1 + u2 + l4 + l2 + u3 + r2 + u4 + r4 + \
+               b1 + b2 + b3 + b4 + r3 + r1 + d3 + d4 + l1 + d1 + l3 + d2
     elif notation == "F2":
-        perm = [f4, f3, f2, f1, u1, u2, d2, d1, l4, r2, l2, r4,
-                b1, b2, b3, b4, u4, u3, d3, d4, l1, r3, l3, r1]
+        perm = f4 + f3 + f2 + f1 + u1 + u2 + d2 + d1 + l4 + r2 + l2 + r4 + \
+               b1 + b2 + b3 + b4 + u4 + u3 + d3 + d4 + l1 + r3 + l3 + r1
     elif notation == "F'":
-        perm = [f2, f4, f1, f3, u1, u2, r1, r3, d2, r2, d1, r4,
-                b1, b2, b3, b4, l2, l4, d3, d4, l1, u4, l3, u3]
-    elif notation == "U":
-        perm = [r1, r2, f3, f4, u3, u1, u4, u2, b1, b2, r3, r4,
-                l1, l2, b3, b4, d1, d2, d3, d4, f1, f2, l3, l4]
+        perm = f2 + f4 + f1 + f3 + u1 + u2 + r1 + r3 + d2 + r2 + d1 + r4 + \
+               b1 + b2 + b3 + b4 + l2 + l4 + d3 + d4 + l1 + u4 + l3 + u3
+    elif notation == "U ":
+        perm = r1 + r2 + f3 + f4 + u3 + u1 + u4 + u2 + b1 + b2 + r3 + r4 + \
+               l1 + l2 + b3 + b4 + d1 + d2 + d3 + d4 + f1 + f2 + l3 + l4
     elif notation == "U2":
-        perm = [b1, b2, f3, f4, u4, u3, u2, u1, l1, l2, r3, r4,
-                f1, f2, b3, b4, d1, d2, d3, d4, r1, r2, l3, l4]
+        perm = b1 + b2 + f3 + f4 + u4 + u3 + u2 + u1 + l1 + l2 + r3 + r4 + \
+               f1 + f2 + b3 + b4 + d1 + d2 + d3 + d4 + r1 + r2 + l3 + l4
     elif notation == "U'":
-        perm = [l1, l2, f3, f4, u2, u4, u1, u3, f1, f2, r3, r4,
-                r1, r2, b3, b4, d1, d2, d3, d4, b1, b2, l3, l4]
-    elif notation == "R":
-        perm = [f1, d2, f3, d4, u1, f2, u3, f4, r3, r1, r4, r2,
-                u4, b2, u2, b4, d1, b3, d3, b1, l1, l2, l3, l4]
+        perm = l1 + l2 + f3 + f4 + u2 + u4 + u1 + u3 + f1 + f2 + r3 + r4 + \
+               r1 + r2 + b3 + b4 + d1 + d2 + d3 + d4 + b1 + b2 + l3 + l4
+    elif notation == "R ":
+        perm = f1 + d2 + f3 + d4 + u1 + f2 + u3 + f4 + r3 + r1 + r4 + r2 + \
+               u4 + b2 + u2 + b4 + d1 + b3 + d3 + b1 + l1 + l2 + l3 + l4
     elif notation == "R2":
-        perm = [f1, b3, f3, b1, u1, d2, u3, d4, r4, r3, r2, r1,
-                f4, b2, f2, b4, d1, u2, d3, u4, l1, l2, l3, l4]
-    else:   # elif notation == "R'":
-        perm = [f1, u2, f3, u4, u1, b3, u3, b1, r2, r4, r1, r3,
-                d4, b2, d2, b4, d1, f2, d3, f4, l1, l2, l3, l4]
+        perm = f1 + b3 + f3 + b1 + u1 + d2 + u3 + d4 + r4 + r3 + r2 + r1 + \
+               f4 + b2 + f2 + b4 + d1 + u2 + d3 + u4 + l1 + l2 + l3 + l4
+    else:  # notation == "R'":
+        perm = f1 + u2 + f3 + u4 + u1 + b3 + u3 + b1 + r2 + r4 + r1 + r3 + \
+               d4 + b2 + d2 + b4 + d1 + f2 + d3 + f4 + l1 + l2 + l3 + l4
     return perm
 
 
 def is_cube_solved(perm):
     for s in range(0, 24, 4):
-        if not (perm[s] == perm[s+1] == perm[s+2] == perm[s+3]):
+        if perm[s] * 4 != perm[s:s+4]:
             return False
     return True
 
@@ -174,23 +188,22 @@ def scan(paths, perms):
     new_perms = []
     for n in range(len(paths)):
         for notation in NOTATIONS:
-            if paths[n][-1][0] != notation[0]:
-                new_path = [notation for notation in paths[n]]
-                new_path.append(notation)
+            if paths[n][-2] != notation[0]:
+                new_path = paths[n]
+                new_path += notation
                 new_perm = produce_permutation(notation, perms[n])
                 new_paths.append(new_path)
                 new_perms.append(new_perm)
                 if is_cube_solved(new_perm):
-                    print(f"\nsolved in {((pygame.time.get_ticks() - last_time) // 100) / 10} seconds")
                     return None, None, new_path
     return new_paths, new_perms, None
 
 
-def solve_the_cube(perm):
+def find_solution(perm):
     if is_cube_solved(perm):
         return []
-    paths = [[notation] for notation in NOTATIONS]
-    perms = [produce_permutation(path[-1], perm) for path in paths]
+    paths = [notation for notation in NOTATIONS]
+    perms = [produce_permutation(path, perm) for path in paths]
     for n in range(len(paths)):
         if is_cube_solved(perms[n]):
             return paths[n]
@@ -198,6 +211,7 @@ def solve_the_cube(perm):
     for _ in range(10):
         paths, perms, solution_path = scan(paths, perms)
         if solution_path is not None:
+            print(f"\nsolved in {((pygame.time.get_ticks() - last_time) // 100) / 10} seconds")
             return solution_path
         print(f"{_+2}) {((pygame.time.get_ticks() - last_time) // 100) / 10}")
 
@@ -237,10 +251,10 @@ for i in range(18):
                         BUTTON_COLOR_1, BUTTON_COLOR_2, BG_COLOR, W // 256)
     turner_buttons.append(new_button)
 
-scramble = []
+scramble = ""
 last_scrambled_permutation = None
 
-solution = []
+solution = ""
 last_solution_permutations = []
 
 last_permutation_solved = None
@@ -257,10 +271,9 @@ button_scramble = Button(screen, "Scramble", gui_font, 13 * W // 16, 35 * W // 6
 button_solve = Button(screen, "Solve", gui_font, 13 * W // 16, 41 * W // 64, W // 8, W // 16,
                       BUTTON_COLOR_1, BUTTON_COLOR_2, BG_COLOR, W // 64)
 
-DEFAULT_PERMUTATION = ['g', 'g', 'g', 'g', 'w', 'w', 'w', 'w', 'r', 'r', 'r', 'r',
-                       'b', 'b', 'b', 'b', 'y', 'y', 'y', 'y', 'o', 'o', 'o', 'o']
+DEFAULT_PERMUTATION = "ggggwwwwrrrrbbbbyyyyoooo"
 
-current_permutation = [c for c in DEFAULT_PERMUTATION]
+current_permutation = DEFAULT_PERMUTATION
 
 while True:
 
@@ -271,20 +284,21 @@ while True:
 
     for turner_button in turner_buttons:
         if turner_button.is_clicked():
-            current_permutation = produce_permutation_for_turner_buttons(turner_button.text, current_permutation)
+            current_permutation = "".join(produce_permutation_for_turner_buttons
+                                          (turner_button.text, current_permutation))
             if current_permutation == last_scrambled_permutation:
-                scramble_surf = gui_font_large.render('  '.join(scramble), True, TEXT_COLOR_1_1)
+                scramble_surf = gui_font_large.render(scramble, True, TEXT_COLOR_1_1)
             else:
-                scramble_surf = gui_font_large.render('  '.join(scramble), True, TEXT_COLOR_1_2)
+                scramble_surf = gui_font_large.render(scramble, True, TEXT_COLOR_1_2)
             if current_permutation in last_solution_permutations:
-                solution_surf = gui_font_large.render('  '.join(solution), True, TEXT_COLOR_2_1)
+                solution_surf = gui_font_large.render(solution, True, TEXT_COLOR_2_1)
             else:
-                solution_surf = gui_font_large.render('  '.join(solution), True, TEXT_COLOR_2_2)
+                solution_surf = gui_font_large.render(solution, True, TEXT_COLOR_2_2)
 
     if button_reset.is_clicked():
-        current_permutation = [c for c in DEFAULT_PERMUTATION]
-        scramble = []
-        solution = []
+        current_permutation = DEFAULT_PERMUTATION
+        scramble = ""
+        solution = ""
         last_scrambled_permutation = None
         last_solution_permutations = []
         last_permutation_solved = None
@@ -294,27 +308,29 @@ while True:
     if button_scramble.is_clicked():
         scramble = produce_random_scramble()
         current_permutation = produce_permutation_from_path(DEFAULT_PERMUTATION, scramble)
-        last_scrambled_permutation = [c for c in current_permutation]
-        scramble_surf = gui_font_large.render('  '.join(scramble), True, TEXT_COLOR_1_1)
+        last_scrambled_permutation = current_permutation
+        scramble = convert_path_into_display_text(scramble)
+        scramble_surf = gui_font_large.render(scramble, True, TEXT_COLOR_1_1)
         scramble_rect = scramble_surf.get_rect(center=(3 * W // 8, 79 * W // 128))
-        solution = []
+        solution = ""
         last_solution_permutations = []
         solution_surf = gui_font_large.render("", True, TEXT_COLOR_2_1)
 
     if button_solve.is_clicked():
         if not is_cube_solved(current_permutation) and current_permutation != last_permutation_solved:
             last_time = pygame.time.get_ticks()
-            solution = solve_the_cube(current_permutation)
+            solution = find_solution(current_permutation)
             last_solution_permutations = produce_permutations_from_path(current_permutation, solution)
-            last_permutation_solved = [c for c in current_permutation]
-            solution_surf = gui_font_large.render('  '.join(solution), True, TEXT_COLOR_2_1)
+            last_permutation_solved = current_permutation
+            solution = convert_path_into_display_text(solution)
+            solution_surf = gui_font_large.render(solution, True, TEXT_COLOR_2_1)
             solution_rect = solution_surf.get_rect(center=(3 * W // 8, 89 * W // 128))
             if current_permutation != last_scrambled_permutation:
-                scramble = []
+                scramble = ""
                 last_scrambled_permutation = None
                 scramble_surf = gui_font_large.render("", True, TEXT_COLOR_1_1)
-        elif is_cube_solved(current_permutation) and [] == scramble:
-            solution = []
+        elif is_cube_solved(current_permutation) and "" == scramble:
+            solution = ""
             last_solution_permutations = []
             solution_surf = gui_font_large.render("", True, TEXT_COLOR_2_1)
 
